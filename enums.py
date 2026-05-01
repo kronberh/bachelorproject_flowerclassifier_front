30b939eff15842871662edaf96ea2019c1f24611
yekro1001 <yekro1001@student.nord.no>
2026-05-01 12:19:35 +0200

    fixed typo

--- enums.py
+++ enums.py
@@ -1,11 +1,11 @@
 from enum import Enum
 
-class AnswerAwarenessStruct():
+class AnswerCompletenessStruct():
     def __init__(self, id: int, name: str):
         self.id = id
         self.name = name
 
 class AnswerCompleteness(Enum):
-    NAME = AnswerAwarenessStruct(0, 'Only name')
-    SHORT = AnswerAwarenessStruct(1, 'Short description')
-    FULL = AnswerAwarenessStruct(2, 'Full description')
+    NAME = AnswerCompletenessStruct(0, 'Only name')
+    SHORT = AnswerCompletenessStruct(1, 'Short description')
+    FULL = AnswerCompletenessStruct(2, 'Full description')
